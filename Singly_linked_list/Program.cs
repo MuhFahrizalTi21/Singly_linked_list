572663f678b0783c5b547df614e057758ae3512e
Muhammad  Fahrizal <m.fahrizal.ft21@mail.umy.ac.id>
2022-11-07 16:50:24 +0700

    Add variable in class node

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace Singly_linked_list
 {
+    //each node consist of the information part and link to the next node
     class Node
     {
-
+        public int rollNumber;
+        public string name;
+        public Node next;
     }
     class List
     {
